5c9abb5a610e3ee571acf3fcfb6e0246e9ac4a9a
dev04 <dev04@yisker.net>
2018-01-16 16:15:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dev04/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.dev04.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
